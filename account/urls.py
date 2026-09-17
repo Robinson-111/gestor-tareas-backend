198d91d66e356b19f00ed2412ad648b28748dca3
DEV_Robinson <robinsonherreracaviedes.2@gmail.com>
2026-09-16 19:14:56 -0500

    feat(account) Creacion de endpoint para listar usuarios

---
+++ account/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from account.views import UserListView, UserDetailView
+
+urlpatterns = [
+    path('', UserListView.as_view(), name = 'users'),
+    path('<int:id>', UserDetailView.as_view(), name = 'user')
+]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from account.models import User
+from account.serializers.user import UserSerializer
 
-# Create your views here.
+
+class UserListView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        
+        #pasar serializer con many=True
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UserDetailView(APIView):
+    def get(self, request, id):
+        user = get_object_or_404(User, id=id)
+
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, id):
+        user_to_delete = get_object_or_404(User, id=id)
+        user_to_delete.delete()
+
+        serializer = UserSerializer(user_to_delete)
+        return Response(
+            {"message": "Usuario eliminado exitosamente"},
+            status=status.HTTP_200_OK
+        )
